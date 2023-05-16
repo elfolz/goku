4d17cd17ea3807e21334a729a2f4ca7962be1ddc
Luiz <lhvalentim@outlook.coom>
2023-05-16 09:14:52 -0300

    fix

--- js/fbxLoader.module.js
+++ js/fbxLoader.module.js
@@ -42,7 +42,7 @@ import {
 	SRGBColorSpace
 } from './three.module.js';
 import * as fflate from './fflate.module.js';
-import { NURBSCurve } from './NURBSCurve.js';
+import { NURBSCurve } from './nurbsCurve.module.js';
 
 /**
  * Loader loads FBX file and generates Group representing FBX scene.
